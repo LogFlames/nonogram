ab8586e455d1b42589a92c1f72b38bfc26169eff
Elias Lundell <elilun03@gmail.com>
2022-05-25 00:58:27 +0200

    Make working binary brute solution

--- nonogram_brute.py
+++ nonogram_brute.py
@@ -1,16 +1,17 @@
 from nonogram_general import check
 
-from nonograms import small_5x5_002 as ng
+def solve(rows, cols):
+    grid = [[ False for _ in range(len(rows))] for _ in range(len(cols))]
 
-grid = [[ False for _ in range(len(ng.rows))] for _ in range(len(ng.cols))]
+    for i in range(0, 2**(len(cols) * len(rows))):
+        if not i % 100000:
+            print(f"{int(i / 2**(len(cols) * len(rows)) * 10000.0) / 100.0}%")
+        for y in range(len(rows)):
+            for x in range(len(cols)):
+                grid[x][y] = True if i & (1 << (y * len(cols) + x)) else False
 
-for i in range(0, 2**(len(ng.cols) * len(ng.rows))):
-    if not i % 100000:
-        print(f"{int(i / 2**(len(ng.cols) * len(ng.rows)) * 10000.0) / 100.0}%")
-    for y in range(len(ng.rows)):
-        for x in range(len(ng.cols)):
-            grid[x][y] = True if i & (1 << (y * len(ng.cols) + x)) else False
+        if check(rows = rows, cols = cols, grid = grid):
+            return grid
 
-    if check(rows = ng.rows, cols = ng.cols, grid = grid):
-        print(grid)
+    return None
 
